609360f8dab72413fb9e3aac972cd882594653e4
Nikolina Marinić <61114828+Nina621@users.noreply.github.com>
2020-11-12 14:31:09 +0100

    Add files via upload

---
+++ Models/OurDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCLoginRegistration.Models
+{
+    public class OurDbContext : DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+
+    }
+}
